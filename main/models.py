a2f2726043ad3c1dedcfa68d8f87d19d7c198b94
Arnav Choudhury <arnav13@gmail.com>
2019-07-30 00:24:49 +0530

    Reformatting.

--- main/models.py
+++ main/models.py
@@ -1,26 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Author(models.Model):
-
-    name = models.CharField(max_length = 256)
+    name = models.CharField(max_length=256)
 
     def __str__(self):
         return self.name
 
 
-
 class Article(models.Model):
-
-    title = models.CharField(max_length = 256)
+    title = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
     thumbnail = models.ImageField(default='static/main/images/article_generic.jpeg')
     authors = models.ManyToManyField("Author")
-
 
     @property
     def get_text_preview(self):
@@ -28,4 +25,3 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
-
--- main/views.py
+++ main/views.py
@@ -4,12 +4,11 @@ from main import models
 
 # Create your views here.
 def Index(request):
-
     latest_articles = models.Article.objects.all().order_by('-created_at')[:10]
     context = {'latest_articles': latest_articles}
     return render(request, "main/index.html", context)
 
 
 def Article(request, article_id):
-    context= {'id': article_id}
+    context = {'id': article_id}
     return render(request, 'main/components/article.html', context)
