5833e2c08d794cbd537c3100d329db6d81a46f20
Arnav Choudhury <arnav13@gmail.com>
2019-08-02 13:07:21 +0530

    Creates a sign up page using django Model Forms. Added validation and an alert if the user signs up succesfully. Improved styling of sign up form.

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from main import models
 
-
 class SignupForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(), min_length=8)
     confirm_Password = forms.CharField(widget=forms.PasswordInput(), min_length=8)
