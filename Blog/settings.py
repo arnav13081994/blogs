ede15390016817046bac16e56bc96c15baeb186c
Arnav Choudhury <arnav13@gmail.com>
2019-08-04 14:22:24 +0530

    Added encryption to login and Sign up forms.

--- Blog/settings.py
+++ Blog/settings.py
@@ -59,8 +59,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-
-
             os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
